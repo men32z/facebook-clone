97e1a05eb5e481ac00c039bb2aca72262a879621
fedgut <fedgut@gmail.com>
2019-12-11 16:39:45 -0600

    add basic test blueprint for pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,4 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
+  describe 'get#index' do
+    expect 'Should show index' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 end

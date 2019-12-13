9d674e56825865e2152ca79ae8f290992052877c
Luis Preza <preza.luis@gmail.com>
2019-12-13 10:07:42 -0600

    create method post, and tests controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
 
-  def create; end
+  def create
+    post = Post.new(post_params)
+    post.user_id = current_user.id
+    if post.save
+      flash.now[:success] = "Post created"
+      redirect_to user_path(current_user)
+    else
+      flash.now[:danger] = "Some errors"
+      redirect_to user_path(current_user)
+    end
+  end
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  
+  let(:user_valid) do
+    has_x = { name: 'Mike', email: 'mike@monstersinc.com' }
+    has_x[:password] = 'wasausky'
+    has_x[:password_confirmation] = 'wasausky'
+    has_x
+  end
+
+  describe 'POST#create' do
+    it 'creates a post' do
+      user = User.create(user_valid)
+      sign_in user
+      content = 'content example'
+      post :create, params: {post: { content: content } }
+      expect(response).to have_http_status(302)
+      expect(Post.last.content).to eq(content)
+    end
+  end
 end

2956502dc9d6e5b3f437c255b93ba5d778c5c00d
fedgut <fedgut@gmail.com>
2019-12-16 10:08:24 -0600

    Add index controller and view to show existing users. Change order of post to show the most recent first in the user profile and timeline

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   def index
     if user_signed_in?
       @post = Post.new
-      @posts = Post.all
+      @posts = Post.order(:id).reverse_order
       render 'timeline'
     else
       render 'index'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
   resources :posts, only: [:create]
   root to: 'pages#index'
 

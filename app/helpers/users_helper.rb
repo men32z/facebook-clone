7e01e5d24665620123c47b07f899d89acb102deb
fedgut <fedgut@gmail.com>
2019-12-19 15:14:53 -0600

    Remove logic frome views and moved into helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def edit_your_profile(user)
+    user_signed_in? && current_user.id == user.id
+  end
+
+  def pending_request?(user)
+    current_user.pending_friend?(user)
+  end
+
+  def existing_relation(user)
+    current_user.related?(user) || current_user.id == user.id
+  end
 end

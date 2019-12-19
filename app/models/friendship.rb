1a8b26d149d91f669197a6f3f9d3f97630c217fa
fedgut <fedgut@gmail.com>
2019-12-19 15:04:50 -0600

    Fix the posibility of repeated friend request sent and created relevant test

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class Friendship < ApplicationRecord
+  validate :users_are_not_already_friends, on: :create
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  def users_are_not_already_friends
+    reverse_friendship = Friendship.where(user_id: friend_id, friend_id: user_id).exists?
+    reg_friendship = Friendship.where(user_id: user_id, friend_id: friend_id).exists?
+    errors.add(:user_id, 'Already friends!') if reverse_friendship || reg_friendship
+  end
 end
